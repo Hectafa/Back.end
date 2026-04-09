9e273bf60e2b46144e54cf6df80c1f3f77e04fa2
Hectafa <hectafa.123@gmail.com>
2026-04-09 15:23:16 -0600

    controlers

--- ecommerce-api/src/controllers/wishListController.js
+++ ecommerce-api/src/controllers/wishListController.js
@@ -2,38 +2,21 @@ import express from "express";
 import WishList from "../models/WishList.js";
 
 
-async function getWishLists(req, res) {
+async function getWishLists(req, res, next) {
     try {
         const wishLists = await WishList.find()
             .populate("user")
             .populate("products.product");
 
-        res.json(wishLists);
+        res.status(200).json(wishLists);
     } catch (error) {
-        console.error(error);
-    }
-};
-
-async function getWhishListById(req, res) {
-    try {
-        const id = req.params.id;
-        const wishList = await WishList.findById(id)
-            .populate("user")
-            .populate("products.product");
-
-        if (!wishList) {
-            return res.status(404).json({ error: "WishList not found" });
-        }
-
-        res.json(wishList);
-    } catch (error) {
-        console.error(error);
+        next(error);
     }
 };
 
 async function getWishListByUser(req, res) {
     try {
-        const userId = req.params.id;
+        const { id } = req.params;
         const wishList = await WishList.findOne({ user: userId })
             .populate("user")
             .populate("products.product");
@@ -42,23 +25,83 @@ async function getWishListByUser(req, res) {
             return res.status(404).json({ error: "No wish list found for this user" });
         };
 
-        res.json(wishList);
+        res.status(200).json(wishList);
     } catch (error) {
-        console.error(error);
+        next(error);
     }
 };
 
-async function createWishList(req, res) {
+async function addProductToWishList(req, res, next) {
     try {
-        const { user, products } = req.body;
+        const { userId, productId } = req.body;
+        let wishlist = await WishList.findOne({ user: userId });
 
-        if (!user || !products || !Array.isArray(products)) {
-            return res.status(400).json({ error: "User and products are required" });
+        if (!wishlist) {
+            wishlist = new WishList({ user: userId, products: [productId] });
+        } else {
+            const alreadyAdded = wishlist.products.some(
+                (p) => p.toString() === productId
+            );
+            if (alreadyAdded) {
+                return res
+                    .status(200)
+                    .json({ message: 'Product already in wishlist' });
+            }
+            wishlist.products.push(productId);
         }
 
-        const newWishList = await WishList.create({ user, products });
-        res.status(201).json(newWishList);
+        await wishlist.save();
+        await wishlist.populate('user');
+        await wishlist.populate('products');
+        res.status(200).json(wishlist);
     } catch (error) {
-        console.error(error);
+        next(error);
     }
-}
+};
+
+async function removeProductFromWishList(req, res, next) {
+    try {
+        const { id } = req.params;
+        const { productId } = req.body;
+        const wishlist = await WishList.findById(id);
+
+        if (!wishlist) {
+            return res.status(404).json({ message: 'Wish list not found' });
+        }
+        wishlist.products = wishlist.products.filter(
+            (p) => p.toString() !== productId
+        );
+
+        await wishlist.save();
+        await wishlist.populate('user');
+        await wishlist.populate('products');
+
+        res.status(200).json(wishlist);
+    } catch (error) {
+        next(error);
+    }
+};
+
+async function deleteWishList(req, res, next) {
+    try {
+        const { id } = req.params;
+        const wishlist = WishList.findByIdAndDelete(id);
+        if (!wishlist) {
+            return res.status(404).json({ message: 'Wish list not found' });
+        }
+
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+};
+
+export {
+    getWishLists,
+    getWishListByUser,
+    addProductToWishList,
+    removeProductFromWishList,
+    deleteWishList,
+};
+
+
